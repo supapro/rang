927f95aed4e6a28a68b58c0dc61ac1f647426145
Pavel Kalugin <paul.kalug@gmail.com>
2020-12-07 00:30:50 +0300

    Add ncurses hello world app
    - Bump CMake version to 3.17
    - Require ncurses
    - Include ncurses libraries
    
    Signed-off-by: Pavel Kalugin <paul.kalug@gmail.com>

--- src/rang.cpp
+++ src/rang.cpp
@@ -1,8 +1,12 @@
 #include "rang.h"
-#include <iostream>
+#include <ncurses.h>
 
 int main(int argc, char *argv[])
 {
-        std::cout << rang::MESSAGE;
+        initscr();                     /* Start curses mode 		  */
+        printw(rang::MESSAGE.c_str()); /* Print Hello World		  */
+        refresh();                     /* Print it on to the real screen */
+        getch();                       /* Wait for user input */
+        endwin();                      /* End curses mode		  */
         return 0;
 }
